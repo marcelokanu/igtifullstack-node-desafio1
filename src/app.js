b5982baa9d063707cef495edaadc25b193f6182a
marcelokanu <marcelokanu@gmail.com>
2020-06-04 00:48:58 -0400

    finished

--- src/app.js
+++ src/app.js
@@ -1,139 +1,176 @@
 const express = require("express");
 const fs = require("fs");
 const { promisify } = require("util");
+const path = require("path");
 
 const app = express();
 
 const readFileAsync = promisify(fs.readFile);
 const writeFileAsync = promisify(fs.writeFile);
 
-let estados = [];
-let cidades = [];
 
 const CidadesEstado = [];
 
-const loadFiles = async () => {
-  const fileStates = await readFileAsync("./src/json/Estados.json");
-  estados = JSON.parse(fileStates);
-  const fileCitys = await readFileAsync("./src/json/Cidades.json");
-  cidades = JSON.parse(fileCitys);
-};
+let estados = require(path.resolve(__dirname, 'json', 'estados.json'));
+let cidades = require(path.resolve(__dirname, 'json', 'cidades.json'));
 
-loadFiles();
+async function start() {
+  await loadCidadesEstado();
+   maisCidades();
+   menosCidades();
+   maiorMenorCidade();
+}
+
+
+const loadCidadesEstado = async () => {
+  await estados.map((estado) => {
+    let cidadesDoEstado = cidades.filter(
+      (cidade) => estado.ID === cidade.Estado
+    );
+    cidadesDoEstado = cidadesDoEstado.map((item) => {
+      return {nome: item.Nome, tamanhostring: item.Nome.length};
+    });
+    cidadesDoEstado.sort((a,b) => a.nome.localeCompare(b.nome));
+    //5 e 6
+    const maiorString = Math.max.apply(Math, cidadesDoEstado.map(item => item.tamanhostring))
+    const cidadeMaiorString = cidadesDoEstado.find(cidade => cidade.tamanhostring === maiorString)
+    const menorString = Math.min.apply(Math, cidadesDoEstado.map(item => item.tamanhostring))
+    const cidadeMenorString = cidadesDoEstado.find(cidade => cidade.tamanhostring === menorString)
+
+    CidadesEstado.push({
+      uf: estado.Sigla,
+      totalcidades: cidadesDoEstado.length,
+      maiorCidade:  [
+        cidadeMaiorString,
+        maiorString,
+      ],
+      menorCidade: [
+        cidadeMenorString,
+        menorString,
+      ]
+    });
+    fs.exists(
+      path.resolve(__dirname, "json", "estados", `${estado.Sigla}.json`),
+      (data) => {
+        if (!data) {
+          generateJsonEstados(
+            estado.Sigla,
+            cidadesDoEstado.length,
+            cidadesDoEstado,
+          );
+        }
+      }
+    );
+  });
+};
 
 app.get("/estados", (_, res) => {
-  res.json(estados);
+  return res.json(estados);
 });
 
-const loadFile = (uf) => {
-  const file = fs.readFileSync(`./src/json/estados/${uf}.json`);
-  ufJson = JSON.parse(file);
-  return {"uf": uf,"totalcidades": ufJson.totalcidades, "cidades": ufJson.cidades};
-};
+app.get("/cidades", (_, res) => {
+  return res.json(cidades);
+});
 
-//1 - Implementar um método que irá criar um arquivo JSON para cada estado representado no arquivo Estados.json, e o seu conteúdo será um array das cidades pertencentes aquele estado, de acordo com o arquivo Cidades.json. O nome do arquivo deve ser o UF do estado, por exemplo: MG.json.
+
+
+app.get("/CidadesEstado", (_, res) => {
+  res.json(CidadesEstado);
+});
+
 const generateJsonEstados = async (uf, totalcidades, cidades) => {
   try {
     await writeFileAsync(
-      `./src/json/estados/${uf}.json`,
+      path.resolve(__dirname, "json", "estados", `${uf}.json`),
       JSON.stringify({ uf, totalcidades, cidades }),
-      (err) => {
-        console.log(err);
-      }
     );
   } catch (err) {
     console.log(err);
   }
 };
 
-//2 - Criar um método que recebe como parâmetro o UF do estado, realize a leitura do arquivo JSON correspondente e retorne a quantidade de cidades daquele estado.
-app.get("/estados/:uf", (req, res) => {
-  return res.json(loadFile(req.params.uf));
-});
-
-//3 - Criar um método que imprima no console um array com o UF dos cinco estados que mais possuem cidades, seguidos da quantidade, em ordem decrescente. Utilize o método criado no tópico anterior. Exemplo de impressão: [“UF - 93”, “UF - 82”, “UF - 74”, “UF - 72”, “UF - 65”]
-app.get("/top5", (_, res) => {
-  loadCidadesEstado()
-  const maiorQtdeCidades = CidadesEstado.map((item) => {
-    return {
-      uf: item.uf,
-      totalcidades: item.totalcidades,
-    }
-  });
- 
-  maiorQtdeCidades.sort((a,b) => {
-    return a.totalcidades > b.totalcidades ? -1 : a.totalcidades < b.totalcidades ? 1 : 0;
-  })
-
-  console.log(["5 mais quantidade de cidades", maiorQtdeCidades.slice(0, 5)]);
-  return res.json(["5 mais quantidade de cidades", maiorQtdeCidades.slice(0, 5)]);
-});
-//4 - Criar um método que imprima no console um array com o UF dos cinco estados que menos possuem cidades, seguidos da quantidade, em ordem decrescente. Utilize o método criado no tópico anterior. Exemplo de impressão: [“UF - 30”, “UF - 27”, “UF - 25”, “UF - 23”, “UF - 21”]
-app.get("/min5", (_, res) => {
-  loadCidadesEstado()
-  const menorQtdeCidades = CidadesEstado.map((item) => {
-    return {
-      uf: item.uf,
-      totalcidades: item.totalcidades,
-    }
-  });
- 
-  menorQtdeCidades.sort((a,b) => {
-    return a.totalcidades < b.totalcidades ? -1 : a.totalcidades > b.totalcidades ? 1 : 0;
-  }) 
-  console.log(["5 menos quantidade de cidades", menorQtdeCidades.slice(0, 5)]);
-  return res.json(["5 menos quantidade de cidades", menorQtdeCidades.slice(0, 5)]);
-});
-
-//5 - Criar um método que imprima no console um array com a cidade de maior nome de cada estado, seguida de seu UF. Em caso de empate, considerar a ordem alfabética para ordená-los e então retornar o primeiro. Por exemplo: [“Nome da Cidade – UF”, “Nome da Cidade – UF”, ...].
-
-app.get("/maiorcidade", (_, res) => {
-  loadCidadesEstado()
-  const maiorcidade = 0
-  CidadesEstado.map(cidades =>  {
-    cidades.forEach(cidade => {
-      if (cidade.tamanhostring > maiorcidade ) {
-        maiorcidade = cidade.tamanhostring
-      }
-    });
-  })
-  res.json(maiorcidade);
-});
-
-
-app.get("/cidades", (_, res) => {
-  res.json(cidades);
-});
-
-function loadCidadesEstado() {
-  if (CidadesEstado.length !== 0) {
+//2
+const loadUF = async (uf) => {
+  if (uf === undefined) {
     return;
   }
-  estados.map((estado) => {
-    let cidadesDoEstado = cidades.filter(
-      (cidade) => estado.ID === cidade.Estado
-    );
-    cidadesDoEstado = cidadesDoEstado.map((item) => {
-      return item.Nome
-    });
+  const file = fs.readFileSync(
+    path.resolve(__dirname, "json", "estados", `${uf}.json`)
+  );
+  ufJson = JSON.parse(file);
+  return { uf: uf, totalcidades: ufJson.totalcidades };
+};
 
-    CidadesEstado.push({
-      uf: estado.Sigla,
-      totalcidades: cidadesDoEstado.length,
-  
-      cidades: cidadesDoEstado,
-    });
-    fs.exists(`./src/json/estados/${estado.Sigla}.json`, (data) => {
-      if (!data) {
-        generateJsonEstados(estado.Sigla, cidadesDoEstado.length, cidadesDoEstado);
-      }
-    });
+//3
+const maisCidades = async () => {
+  let maiorQtdeCidades = CidadesEstado.map((item) => {
+    return {
+      uf: item.uf,
+      totalcidades: item.totalcidades,
+    };
   });
+  maiorQtdeCidades.sort((a, b) =>
+    a.totalcidades > b.totalcidades
+      ? -1
+      : a.totalcidades < b.totalcidades
+      ? 1
+      : 0
+  );
+
+  maiorQtdeCidades = maiorQtdeCidades.slice(0, 5);
+
+  return ["5 MAIS +++ quantidade de cidades", maiorQtdeCidades];
+};
+
+//4
+const menosCidades = async () => {
+  let menorQtdeCidades = CidadesEstado.map((item) => ({
+    uf: item.uf,
+    totalcidades: item.totalcidades,
+  }));
+  menorQtdeCidades.sort((a, b) =>
+    a.totalcidades < b.totalcidades
+      ? -1
+      : a.totalcidades > b.totalcidades
+      ? 1
+      : 0
+  );
+
+  menorQtdeCidades = menorQtdeCidades.slice(0, 5);
+
+  return ["5 MENOS --- quantidade de cidades", menorQtdeCidades];
+};
+
+//7 e 8
+const maiorMenorCidade = async () => {
+  const cidadesOrdened = cidades.sort((a, b) => a.Nome.localeCompare(b.Nome));
+  const maiorString = Math.max.apply(Math, cidadesOrdened.map(item => item.Nome.length))
+  const cidadeMaiorString = cidadesOrdened.find(Nome => Nome.Nome.length === maiorString)
+  const menorString = Math.min.apply(Math, cidadesOrdened.map(item => item.Nome.length))
+  const cidadeMenorString = cidadesOrdened.find(Nome => Nome.Nome.length === menorString)
+  return [
+    {maior: maiorString, cidadeMaiorString, 
+    menor: menorString, cidadeMenorString}
+  ]
 }
 
-app.get("/CidadesEstado", (_, res) => {
-  loadCidadesEstado()
-  res.json(CidadesEstado);
+app.get("/", async (_, res) => {
+  const maiorMenor = await maiorMenorCidade();
+  const uf = await loadUF("ms");
+  const cincoMais = await maisCidades();
+  const cincoMenos = await menosCidades();
+  res.json({
+    maiorMenor,
+    cincoMais,
+    cincoMenos,
+    uf,
+  });
 });
+
+app.get("/:uf", (req, res) => {
+  return res.json(loadUF(req.params.uf));
+});
+
+start();
 
 module.exports = app;
